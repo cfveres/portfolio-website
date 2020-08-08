9a0dc20acf14e44801d630bf1f270c64b05f5f6b
Chas Veres <Chas@Chass-MacBook-Pro.local>
2020-08-08 14:00:58 -0400

    Fixed NavLists, Fixed Footers, added Contact posting to contact page.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +9,29 @@ def homepage():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+def write_to_file(data):
+    with open('db.txt', mode='a') as db:
+        name = data['name']
+        email = data['email']
+        message = data['message']
+        db.write(f'\n{name},{email},{message}')
+
+def write_to_csv(data):
+    with open('db.csv', newline='', mode='a') as db:
+        name = data['name']
+        email = data['email']
+        message = data['message']
+        csv_writer = csv.writer(db, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name,email,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'Post error: not saved to database.'
+    return 'Something went wrong. Please try again.'
